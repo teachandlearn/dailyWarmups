9a68f2b3838b0f516746c8ee1b01175c965d2572
teachandlearn <60556876+teachandlearn@users.noreply.github.com>
2020-04-14 15:27:44 -0500

    Create switch_two_variables.py

---
+++ switch_two_variables.py
@@ -0,0 +1,17 @@
+# Super easy, but it might be a good activity for my students.
+# Ask: show me (on paper, but better on a whiteboard) how you would swap the values of two variables.
+# https://stackoverflow.com/questions/117812/alternate-fizzbuzz-questions
+
+# Variables
+var_one = 25
+var_two = 35
+placeholder = 0
+
+# Switch the values
+placeholder = var_one
+var_one = var_two
+var_two = placeholder
+
+# output
+print("Variable One: " + str(var_one))
+print("Variable Two: " + str(var_two))
