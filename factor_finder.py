62d0ccf3d2084a774ad77c6689f47f083604fe32
teachandlearn <60556876+teachandlearn@users.noreply.github.com>
2020-04-15 13:29:16 -0500

    Accounted for the number being a factor of itself

--- factor_finder.py
+++ factor_finder.py
@@ -6,7 +6,7 @@ num = 1020
 factors = []
 
 # iterate every whole number up to one half of the given number
-for i in range(1, round(num/2 + 1)):
+for i in range(1, num + 1):
     if num % i == 0:
         factors.append(i)
 
